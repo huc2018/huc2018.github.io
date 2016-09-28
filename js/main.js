0bcbc0ef3985d56d5601daa117a763c4bedaa175
huc2018 <huc@email.arizona.edu>
2016-09-28 00:18:51 -0500

    add scrolling smooth for the navbar

--- js/main.js
+++ js/main.js
@@ -9,3 +9,34 @@ $(document).ready(function () {
         }
     })
 })
+
+
+$(".navbar-dark ul li a[href^='#']").on('click',function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 1000, function(){
+        window.location.hash = hash;
+    });
+});
+
+$("#main-nav a[href^='#']").on('click',function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 1000, function(){
+        window.location.hash = hash;
+    });
+});
+
+$("#cover p a[href^='#']").on('click',function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 1000, function(){
+        window.location.hash = hash;
+    });
+});
